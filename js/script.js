2acc36da8c9ecece1c94c4ddd33e651ff47cc0eb
orrizX <zo292@nyu.edu>
2021-12-14 15:06:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -97,3 +97,21 @@
     };
 }(jQuery));
 // wall
+
+// ----- weather ----- //
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=New York&units=imperial&appid=ab85ba57bbbb423fb62bfb8201126ede", function(data) {
+
+console.log(data);
+
+var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+var temp = Math.floor(data.main.temp);
+
+var weather = data.weather[0].main;
+
+
+$('.icon').attr('src', icon);
+$(".temp").append(temp  + ' &#8457;');
+
+$(".weather").append(weather );
+});
